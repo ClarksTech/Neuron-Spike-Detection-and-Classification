cf4343494d563630952f68ffc3577529aa31d983
ClarksTech <c.rutter50@gmail.com>
2021-12-15 22:58:29 +0000

    97% performance

--- neuron_CNN.py
+++ neuron_CNN.py
@@ -27,9 +27,8 @@ test_waveforms = index_waveforms[split_len:]
 
 # correct dimensions for keras to shape (50,1)
 train_waveforms = np.expand_dims(train_waveforms, -1)
-#train_waveforms = np.expand_dims(train_waveforms, -1)
 test_waveforms = np.expand_dims(test_waveforms, -1)
-#test_waveforms = np.expand_dims(test_waveforms, -1)
+
 
 print("train waveform shape: ", train_waveforms.shape)
 print(train_waveforms.shape[0], " train samples")
@@ -51,11 +50,13 @@ test_class = keras.utils.to_categorical(test_class, num_classes)
 model = keras.Sequential(
     [
         keras.Input(shape=input_shape),
-        layers.Conv1D(32, kernel_size=3, activation="relu"),
+        layers.Conv1D(32, padding="same", kernel_size=7, activation="relu"),
         layers.MaxPooling1D(pool_size=2),
-        layers.Conv1D(64, kernel_size=4, activation="relu"),
+        layers.Conv1D(64, padding="same", kernel_size=5, activation="relu"),
         layers.MaxPooling1D(pool_size=2),
-        layers.Conv1D(64, kernel_size=5, activation="relu"),
+        layers.Conv1D(128, padding="same", kernel_size=3, activation="relu"),
+        layers.MaxPooling1D(pool_size=2),
+        layers.Conv1D(256, padding="same", kernel_size=1, activation="relu"),
         layers.MaxPooling1D(pool_size=2),
         layers.Flatten(),
         layers.Dropout(0.5),
@@ -67,8 +68,8 @@ model = keras.Sequential(
 model.summary()
 
 # decide batches and epoch number
-batch_size = 10
-epochs = 15
+batch_size = 40
+epochs = 30
 
 # compile the model
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
@@ -80,3 +81,21 @@ model.fit(train_waveforms, train_class, batch_size=batch_size, epochs=epochs, va
 score = model.evaluate(test_waveforms, test_class, batch_size =1)
 print("Test loss:", score[0])
 print("Test accuracy:", score[1])
+
+# save the models weights for use later
+model.save_weights('Training_data_CNN.h5')
+
+# make predictions for test set
+training_test_predictions = model.predict(test_waveforms)
+training_test_predictions = np.argmax(training_test_predictions, axis=1)
+# increment class numbers to start from 1 again
+training_test_predictions[:] = [pred + 1 for pred in training_test_predictions]
+test_class = Class[split_len:]
+test_class[:] = [Class + 1 for Class in test_class]
+correct_count = 0
+for x in range(len(test_class)):
+    if test_class[x] == training_test_predictions[x]:
+        correct_count = correct_count + 1
+    
+performance = correct_count/len(test_class)
+print("CNN Predicts the correct class of neuron ", performance, "%")
