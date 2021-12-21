0dbc7e60df2c89c2841ebf185e610531427eab75
ClarksTech <c.rutter50@gmail.com>
2021-12-21 22:16:00 +0000

    non-ideal data used

--- KNN_peak_classifier.py
+++ KNN_peak_classifier.py
@@ -1,7 +1,7 @@
 ######################################################################################
 ############################### - Import Libraries - #################################
 
-import numpy
+import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
@@ -33,6 +33,44 @@ def ideal_data_preperation(data_file):
     return train_waveforms, train_class, test_waveforms, test_class
 
 ######################################################################################
+############## - Non-ideal Data Extraction, Proccessing, Splitting - #################
+def non_ideal_data_preperation(data_file):
+
+    # the data, split to test and train sets
+    data_stream, Index, Class, sample_rate = pd.load_training_dataset(data_file)    # load the ideal training data
+    filtered_data_stream = pd.wavelet_filter_datastream(data_stream)                # filter the waveform
+
+    # detect the peaks in the resulting datastream - store peak index and waveform in variables
+    peak_start_index, peak_found_waveform, peak_maxima_index = pd.convolution_peak_detection(filtered_data_stream, 0.42, 50)
+
+    # print length of known indexes
+    print("known number of peaks: ", len(Index))
+
+    # print length of found indexes
+    print("Detected number of peaks: ", len(peak_maxima_index))
+
+    # get the correct and incorrect peak index lists
+    incorrect_peak_index, correct_peak_index, correct_predict_maxima_index, correct_predict_maxima_class = pm.get_peak_detection_correct_and_incorrect_index(Index, Class, peak_maxima_index)
+
+    # get waveform windows for indexes for ideal indexes
+    correct_predicted_index_waveforms = []
+    # extract window for every known ideal index
+    for x in range(len(correct_predict_maxima_index)):
+        windowmin = correct_predict_maxima_index[x]-25                                             # 25 points before index
+        windowmax = correct_predict_maxima_index[x]+25                                             # 25 points after index
+        correct_predicted_index_waveforms.append(filtered_data_stream[windowmin:windowmax])   # add waveform window to list
+
+    # split 70/30 index and waveforms 70% train 30% test
+    split_len = int(0.70*len(Index))                # define list split point as 85%
+    train_class = correct_predict_maxima_class[:split_len]                 # store first 85% in training class list
+    train_waveforms = correct_predicted_index_waveforms[:split_len]   # store first 85% in training waveforms list
+    test_class = correct_predict_maxima_class[split_len:]                  # store last 15% in test class list
+    test_waveforms = correct_predicted_index_waveforms[split_len:]    # store last 15% in test waveform list
+
+    # return the test and train waveforms and classes
+    return train_waveforms, train_class, test_waveforms, test_class
+
+######################################################################################
 #################################### - PCA - #########################################
 def data_PCA(train_waveforms, test_waveforms, num_PCA_components):
     # Select number of components to extract
@@ -42,7 +80,7 @@ def data_PCA(train_waveforms, test_waveforms, num_PCA_components):
     pca.fit(train_waveforms)
 
     # Determine amount of variance explained by components
-    print("Total Variance Explained: ", numpy.sum(pca.explained_variance_ratio_))
+    print("Total Variance Explained: ", np.sum(pca.explained_variance_ratio_))
 
     # Plot the explained variance
     plt.plot(pca.explained_variance_ratio_)
@@ -82,7 +120,7 @@ def KNN_clasifier(PCA_train_waveforms, train_class, PCA_test_waveforms, test_cla
         pass
 
     # Calculate the performance score, the fraction of correct answers
-    scorecard_array = numpy.asarray(scorecard)
+    scorecard_array = np.asarray(scorecard)
     print("Performance = ", (scorecard_array.sum() / scorecard_array.size) * 100, '%')
 
     test_class_predictions = pred
@@ -96,8 +134,11 @@ def KNN_clasifier(PCA_train_waveforms, train_class, PCA_test_waveforms, test_cla
 test_KNN_performance = 1
 if test_KNN_performance == 1:
 
-    # prepare the ideal data for the KNN - using known peak locations so evaluation is of classification only
-    train_waveforms, train_class, test_waveforms, test_class = ideal_data_preperation("training.mat")
+    # prepare the ideal data for the CNN - using known peak locations so evaluation is of classification only
+    #training_waveforms, training_class, test_waveforms, test_class = ideal_data_preperation("training.mat")
+
+    # prepare the non-ideal data for the CNN - this tests classification performance on correctly identified peak indexes at peak maxima
+    train_waveforms, train_class, test_waveforms, test_class = non_ideal_data_preperation("training.mat")
 
     # find the PCA components of the train and test waveforms for KNN
     PCA_train_waveforms, PCA_test_waveforms = data_PCA(train_waveforms, test_waveforms, num_PCA_components=6)
